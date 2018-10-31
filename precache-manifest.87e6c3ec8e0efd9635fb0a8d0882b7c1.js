90340bf0cb5c882c9f1fdb9d2052f1622c93edcf
diegomendezp <diegomen15@gmail.com>
2018-10-31 12:45:50 +0100

    Updates

---
+++ precache-manifest.87e6c3ec8e0efd9635fb0a8d0882b7c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "189a3550574194e7f75c",
+    "url": "/PreWorkOfPreWork/static/js/runtime~main.189a3550.js"
+  },
+  {
+    "revision": "91a6de34e4cc11fa3752",
+    "url": "/PreWorkOfPreWork/static/js/main.91a6de34.chunk.js"
+  },
+  {
+    "revision": "1ccd896d2998772ddfc5",
+    "url": "/PreWorkOfPreWork/static/js/1.1ccd896d.chunk.js"
+  },
+  {
+    "revision": "91a6de34e4cc11fa3752",
+    "url": "/PreWorkOfPreWork/static/css/main.3cbeebce.chunk.css"
+  },
+  {
+    "revision": "c932b921405973d726a2e56736c843dd",
+    "url": "/PreWorkOfPreWork/index.html"
+  }
+];
